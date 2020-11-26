4b110e020fa4139d89fd157b38ac7d072b69ca26
leandrosilva <leandro.silva@bluesoft.com.br>
2020-11-26 12:47:42 -0300

    corrigindo erro de comp

--- src/main/java/br/com/test/HelloWorld.java
+++ src/main/java/br/com/test/HelloWorld.java
@@ -2,7 +2,7 @@ package br.com.bluesoft.test;
 
 public final class HelloWorld extends ReportIndex {
 
-    public static void main (String arg... a){
+    public static void main (String... arg){
         System.out.println("teste");
     }
 }
