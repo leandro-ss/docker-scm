783e4057172b725fb329a930665eb4bb258ee99a
leandrosilva <leandro.silva@bluesoft.com.br>
2020-11-27 10:34:20 -0300

    removing trash

--- src/main/resources/static/operacao/datavalidadeproduto/datavalidadeproduto.js
+++
@@ -1,6 +0,0 @@
-var label = $("label[for='tipoDeVisualizacao']")[0], $img = $("<img></img>");
-$img.attr('src', 'https://cdn.bluesoft.com.br/commons/icons/informacao.png');
-$img.attr('title', label.title);
-$($img).tooltip();
-label.after(" ");
-label.after($img[0]);
--- src/main/resources/static/vendas/packvirtual/movimento-fornecedor-pack-virtual-report.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function () {
-    $("label:contains('Fornecedor')").attr('for', 'descricaoFornecedor');
-});
